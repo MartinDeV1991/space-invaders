4e252d047fab3580d6a322fc894c3185e848bd73
Martin <martin_de_v@hotmail.com>
2024-02-21 19:20:48 +0100

    enemy types, enemy states, audio

--- enemy.js
+++ enemy.js
@@ -1,58 +1,251 @@
 class Enemy {
-    constructor(game){
+    constructor(game) {
         this.game = game;
-        this.width = 50;
-        this.height = 50;
+        this.spriteWidth = 100;
+        this.spriteHeight = 100;
+        this.sizeModifier = Math.random() * 0.5 + 0.5;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.x;
         this.y;
-        this.speedX = 0;
-        this.speedY = Math.random() * 2 + 0.2;
+        this.speedX;
+        this.speedY;
+        this.frameX
+        this.frameY;
+        this.minFrame;
+        this.maxFrame;
+        this.lastFrame;
         this.lives;
         this.free = true;
     }
-    start(){
+    start() {
         this.x = Math.random() * this.game.width;
         this.y = -this.height;
-        this.lives = 2;
+        this.frameX = 0;
+        this.frameY = Math.floor(Math.random() * 4);
         this.free = false;
     }
-    reset(){
+    reset() {
         this.free = true;
     }
-    isAlive(){
+    isAlive() {
         return this.lives >= 1;
     }
-    update(){
-        if (!this.free){
+    hit() {
+        // check collision
+        if (this.game.checkCollision(this, this.game.mouse) && this.game.mouse.pressed && !this.game.mouse.fired) {
+            this.lives--;
+            this.game.mouse.fired = true;
+        }
+    }
+    update() {
+        if (!this.free) {
             // float in
             if (this.y < 0) this.y += 5;
             // make sure always visible
-            if (this.x > this.game.width - this.width){
+            if (this.x > this.game.width - this.width) {
                 this.x = this.game.width - this.width;
             }
 
             this.x += this.speedX;
             this.y += this.speedY;
 
-            // check collision
-            if (this.game.checkCollision(this, this.game.mouse) && this.game.mouse.pressed && !this.game.mouse.fired){
-                this.lives--;
-                this.game.mouse.fired = true;
-            }
-            if (!this.isAlive()){
+            if (this.y > this.game.height) {
                 this.reset();
-                this.game.score++;
+                if (!this.game.gameOver) this.game.lives--;
+                this.game.sound.scream.play();
             }
-            if (this.y > this.game.height){
-                this.reset();
-                this.game.lives--;
+
+            if (!this.isAlive()) {
+                if (this.game.spriteUpdate) {
+                    this.frameX++
+                    if (this.frameX > this.lastFrame) {
+                        this.reset();
+                        if (!this.game.gameOver) this.game.score++;
+                    }
+                }
             }
         }
     }
-    draw(){
-        if (!this.free){
-             this.game.ctx.strokeRect(this.x, this.y, this.width, this.height);
-            this.game.ctx.fillText(this.lives, this.x + this.width * 0.5, this.y + this.height * 0.5);
+    draw() {
+        if (!this.free) {
+            this.game.ctx.drawImage(this.image,
+                this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight,
+                this.x, this.y, this.width, this.height);
+            if (this.game.debug) {
+                this.game.ctx.strokeRect(this.x, this.y, this.width, this.height);
+                this.game.ctx.fillText(this.lives, this.x + this.width * 0.5, this.y + this.height * 0.5);
+            }
         }
+    }
+}
+
+class BeetleMorph extends Enemy {
+    constructor(game) {
+        super(game);
+        this.image = document.getElementById('beetle');
+        this.lastFrame = 3;
+    }
+    start() {
+        super.start()
+        this.speedX = 0;
+        this.speedY = Math.random() * 2 + 0.2;
+        this.lives = 1;
+    }
+    update() {
+        super.update();
+        if (!this.free) {
+            if (this.isAlive()) {
+                this.hit();
+            }
+        }
+    }
+}
+
+class Lobstermorph extends Enemy {
+    constructor(game) {
+        super(game);
+        this.image = document.getElementById('lobster');
+        this.lastFrame = 14;
+    }
+    start() {
+        super.start()
+        this.speedX = 0;
+        this.speedY = Math.random() * 0.5 + 0.2;
+        this.lives = 3;
+
+    }
+    update() {
+        super.update();
+        if (!this.free) {
+            if (this.lives >= 3) {
+                this.maxFrame = 0;
+            } else if (this.lives === 2) {
+                this.maxFrame = 3;
+            } else if (this.lives === 1) {
+                this.maxFrame = 7;
+            }
+            if (this.isAlive()) {
+                this.hit();
+                if (this.frameX < this.maxFrame && this.game.spriteUpdate) {
+                    this.frameX++;
+                }
+            }
+        }
+    }
+}
+
+class Phantommorph extends Enemy {
+    constructor(game) {
+        super(game);
+        this.image = document.getElementById('phantom');
+        this.lastFrame = 14;
+        this.states = [new Flying(game, this), new Phasing(game, this), new Imploding(game, this)];
+        this.currentState;
+        this.switchTimer = 0;
+        this.switchInterval = 2000;
+        this.switchInterval = Math.random() * 2000 + 1000;
+    }
+    start() {
+        super.start()
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 0.5 + 0.2;
+        this.lives = 1;
+        this.setState(Math.floor(Math.random() * 2));
+    }
+    setState(state) {
+        this.currentState = this.states[state];
+        this.currentState.start();
+    }
+    handleFrames() {
+        if (this.game.spriteUpdate) {
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            } else {
+                this.frameX = this.minFrame;
+            }
+        }
+    }
+    switch() {
+        if (this.currentState === this.states[0]) {
+            this.setState(1);
+        } else {
+            this.setState(0);
+        }
+    }
+    hit() {
+        super.hit();
+        if (!this.isAlive()) this.setState(2);
+    }
+    update(deltaTime) {
+        super.update();
+        if (!this.free) {
+            this.currentState.update();
+            if (this.x <= 0 || this.x >= this.game.width - this.width) {
+                this.speedX *= -1;
+            }
+
+            if (this.isAlive()) {
+                if (this.switchTimer < this.switchInterval) {
+                    this.switchTimer += deltaTime;
+                } else {
+                    this.switchTimer = 0;
+                    this.switch();
+                }
+            }
+        }
+    }
+}
+
+class EnemyState {
+    constructor(game, enemy) {
+        this.game = game;
+        this.enemy = enemy;
+    }
+}
+
+class Flying extends EnemyState {
+    start() {
+        this.enemy.minFrame = 0;
+        this.enemy.maxFrame = 2;
+        this.enemy.speedX = Math.random() * 2 - 1;
+        this.enemy.speedY = Math.random() * 0.5 + 0.2;
+        this.enemy.frameX = this.enemy.minFrame;
+    }
+    update() {
+        this.enemy.hit();
+        this.enemy.handleFrames();
+    }
+}
+
+class Phasing extends EnemyState {
+    start() {
+        this.enemy.minFrame = 3;
+        this.enemy.maxFrame = 5;
+        this.enemy.speedX = Math.random() * 2 - 1;
+        this.enemy.speedY = Math.random() * 0.5 + 0.2;
+        this.enemy.frameX = this.enemy.minFrame;
+    }
+    update() {
+        this.enemy.handleFrames();
+        if (this.game.checkCollision(this.enemy, this.game.mouse) && this.game.mouse.pressed) {
+            this.game.mouse.fired = true;
+            this.enemy.y += 25;
+            this.enemy.speedX = 0;
+            this.enemy.speedY = 2;
+            this.game.sound.play(this.game.sound.slide);
+        }
+    }
+}
+
+class Imploding extends EnemyState {
+    start() {
+        this.enemy.minFrame = 6;
+        this.enemy.maxFrame = this.enemy.lastFrame + 1;
+        this.enemy.frameX = this.enemy.minFrame;
+        this.game.sound.play(this.game.sound.boomSounds[Math.floor(Math.random() * 4)]);
+    }
+    update() {
+
     }
 }
--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+class AudioControl {
+    constructor() {
+        this.newGame = document.getElementById('newgame');
+        this.boom1 = document.getElementById('boom1');
+        this.boom2 = document.getElementById('boom2');
+        this.boom3 = document.getElementById('boom3');
+        this.boom4 = document.getElementById('boom4');
+        this.slide = document.getElementById('slide');
+
+        this.win = document.getElementById('win');
+        this.lose = document.getElementById('lose');
+        this.scream = document.getElementById('scream');
+
+        this.newGame.volume = 0.5;
+        this.win.volume = 0.8;
+        this.lose.volume = 0.8;
+        this.scream.volume = 0.2;
+        this.boom1.volume = 0.9;
+        this.boom2.volume = 0.9;
+        this.boom3.volume = 0.9;
+        this.boom4.volume = 0.9;
+
+        this.boomSounds = [this.boom1, this.boom2, this.boom3, this.boom4];
+    }
+    play(audio) {
+        audio.currentTime = 0;
+        audio.play();
+    }
+}
+
 class Game {
-    constructor(canvas, ctx){
+    constructor(canvas, ctx) {
         this.canvas = canvas;
         this.ctx = ctx;
         this.width = this.canvas.width;
@@ -13,11 +43,19 @@ class Game {
 
         this.score = 0;
         this.lives;
-        this.winningScore = 3;
+        this.winningScore = 20;
         this.message1 = 'Run!';
         this.message2 = 'Or get eaten!';
         this.message3 = 'Press "ENTER" or "R" to start!';
         this.gameOver = true;
+        this.crewImage = document.getElementById('crewSprite');
+        this.debug = false;
+
+        this.spriteTimer = 0;
+        this.spriteInterval = 150;
+        this.spriteUpdate = false;
+
+        this.sound = new AudioControl();
 
         this.mouse = {
             x: undefined,
@@ -27,7 +65,7 @@ class Game {
             pressed: false,
             fired: false
         }
- 
+
         this.resize(window.innerWidth, window.innerHeight);
         this.resetButton = document.getElementById('resetButton');
         this.resetButton.addEventListener('click', e => {
@@ -64,27 +102,37 @@ class Game {
             this.mouse.pressed = false;
         });
         window.addEventListener('keyup', e => {
-            if (e.key === 'Enter' || e.key.toLowerCase() === 'r'){
+            if (e.key === 'Enter' || e.key.toLowerCase() === 'r') {
                 this.start();
-            } else if (e.key === ' ' || e.key.toLowerCase() === 'f'){
+            } else if (e.key === ' ' || e.key.toLowerCase() === 'f') {
                 this.toggleFullScreen();
+            } else if (e.key.toLowerCase() === 'd') {
+                this.debug = !this.debug;
             }
         })
     }
-    start(){
+    start() {
         this.resize(window.innerWidth, window.innerHeight);
         this.score = 0;
-        this.lives = 3;
+        this.lives = 15;
+        this.generateCrew();
         this.gameOver = false;
         this.enemyPool.forEach(enemy => {
             enemy.reset();
         });
-        for (let i = 0; i < 2; i++){
+        for (let i = 0; i < 2; i++) {
             const enemy = this.getEnemy();
             if (enemy) enemy.start();
         }
+        this.sound.newGame.play();
     }
-    resize(width, height){
+    generateCrew() {
+        this.crewMembers = [];
+        for (let i = 0; i < this.lives; i++) {
+            this.crewMembers.push({ frameX: Math.floor(Math.random() * 5), frameY: Math.floor(Math.random() * 5) })
+        }
+    }
+    resize(width, height) {
         this.canvas.width = width;
         this.canvas.height = height;
         this.width = width;
@@ -97,12 +145,12 @@ class Game {
     }
     toggleFullScreen() {
         if (!document.fullscreenElement) {
-          document.documentElement.requestFullscreen();
+            document.documentElement.requestFullscreen();
         } else if (document.exitFullscreen) {
-          document.exitFullscreen();
+            document.exitFullscreen();
         }
-      }
-    checkCollision(rect1, rect2){
+    }
+    checkCollision(rect1, rect2) {
         return (
             rect1.x < rect2.x + rect2.width &&
             rect1.x + rect1.width > rect2.x &&
@@ -110,18 +158,25 @@ class Game {
             rect1.y + rect1.height > rect2.y
         )
     }
-    createEnemyPool(){
-        for (let i = 0; i < this.numberOfEnemies; i++){
-            this.enemyPool.push(new Enemy(this));
+    createEnemyPool() {
+        for (let i = 0; i < this.numberOfEnemies; i++) {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.95) {
+                this.enemyPool.push(new Phantommorph(this));
+            } else if (this.randomNumber < 0.99) {
+                this.enemyPool.push(new Lobstermorph(this));
+            } else if (this.randomNumber <= 1) {
+                this.enemyPool.push(new BeetleMorph(this));
+            }
         }
     }
-    getEnemy(){
-        for (let i = 0; i < this.enemyPool.length; i++){
+    getEnemy() {
+        for (let i = 0; i < this.enemyPool.length; i++) {
             if (this.enemyPool[i].free) return this.enemyPool[i];
         }
     }
-    handleEnemies(deltaTime){
-        if (this.enemyTimer < this.enemyInterval){
+    handleEnemies(deltaTime) {
+        if (this.enemyTimer < this.enemyInterval) {
             this.enemyTimer += deltaTime;
         } else {
             this.enemyTimer = 0;
@@ -129,29 +184,43 @@ class Game {
             if (enemy) enemy.start();
         }
     }
-    triggerGameOver(){
-        if (!this.gameOver){
+    triggerGameOver() {
+        if (!this.gameOver) {
             this.gameOver = true;
-            if (this.lives < 1){
+            if (this.lives < 1) {
                 this.message1 = 'Aargh!';
                 this.message2 = 'The crew was eaten!';
-            } else if (this.score >= this.winningScore){
+                this.sound.play(this.sound.lose);
+            } else if (this.score >= this.winningScore) {
                 this.message1 = 'Well done!';
                 this.message2 = 'You escaped the swarm!';
+                this.sound.play(this.sound.win);
             }
         }
     }
-    drawStatusText(){
+    handleSpriteTimer(deltaTime) {
+        if (this.spriteTimer < this.spriteInterval) {
+            this.spriteTimer += deltaTime;
+            this.spriteUpdate = false;
+        } else {
+            this.spriteTimer = 0;
+            this.spriteUpdate = true;
+        }
+    }
+
+    drawStatusText() {
         this.ctx.save();
         this.ctx.textAlign = 'left';
         this.ctx.fillText('Score: ' + this.score, 20, 40);
-        for (let i = 0; i < this.lives; i++){
-            this.ctx.fillRect(20 + 15 * i, 60, 10, 25);
+        for (let i = 0; i < this.lives; i++) {
+            const w = 20;
+            const h = 45;
+            this.ctx.drawImage(this.crewImage, w * this.crewMembers[i].frameX, h * this.crewMembers[i].frameY, w, h, 20 + 16 * i, 60, w, h);
         }
-        if (this.lives < 1 || this.score >= this.winningScore){
+        if (this.lives < 1 || this.score >= this.winningScore) {
             this.triggerGameOver();
         }
-        if (this.gameOver){
+        if (this.gameOver) {
             this.ctx.textAlign = 'center';
             this.ctx.font = '80px Bangers';
             this.ctx.fillText(this.message1, this.width * 0.5, this.height * 0.5 - 25);
@@ -161,17 +230,20 @@ class Game {
         }
         this.ctx.restore();
     }
-    render(deltaTime){
+    render(deltaTime) {
+        this.handleSpriteTimer(deltaTime);
         this.drawStatusText();
         if (!this.gameOver) this.handleEnemies(deltaTime);
+        for (let i = this.enemyPool.length - 1; i >= 0; i--) {
+            this.enemyPool[i].update(deltaTime);
+        }
         this.enemyPool.forEach(enemy => {
-            enemy.update();
             enemy.draw();
         });
     }
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
@@ -180,7 +252,8 @@ window.addEventListener('load', function(){
     const game = new Game(canvas, ctx);
 
     let lastTime = 0;
-    function animate(timeStamp){
+    // game.start();
+    function animate(timeStamp) {
         const deltaTime = timeStamp - lastTime;
         lastTime = timeStamp;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
