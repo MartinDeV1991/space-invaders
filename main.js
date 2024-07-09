6e0cfc7c49c937241577928bf92459e4592253a1
Martin <martin_de_v@hotmail.com>
2024-07-09 14:17:54 +0200

    added instructions

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ class Game {
         this.score = 0;
         this.lives;
         this.winningScore = 50;
-        this.message1 = 'Run!';
+        this.message1 = 'Destroy the bugs!';
         this.message2 = 'Or get eaten!';
         this.message3 = 'Press "ENTER" or "R" to start!';
         this.gameOver = true;
